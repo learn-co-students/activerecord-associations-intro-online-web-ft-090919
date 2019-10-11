ef432b1471f5bb25b678d480ac8cec4506f84b43
eclecticcoding <chuck@eclecticsaddlebag.com>
2019-10-10 21:16:16 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,19 @@
 
 ActiveRecord::Schema.define(version: 4) do
 
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "genre", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.integer "artist_id"
+    t.integer "genre_id"
+    t.integer "artist"
+  end
+
 end
